213d6e5faecbc66ad2330c74993b2ce35caa165a
GavristovDenis <102148379+GavristovDenis@users.noreply.github.com>
2023-09-02 16:32:10 +0300

    decomposition 2

--- src/pages/checkouts/Checkouts.jsx
+++ src/pages/checkouts/Checkouts.jsx
@@ -1,81 +1,16 @@
 import React, { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import { Link } from "react-router-dom";
+import CheckoutsDropDown from "./components/CheckoutsDropDown";
+import CheckoutsTable from "./components/CheckoutsTable";
 export const Checkouts = () => {
   const [showNavbar, setShowNavbar] = useState(false);
 
   return (
     <div className="checkouts_wrapper">
-      <div className="checkouts_nav_container">
-        <div className="drop_down">
-          <div
-            className="logo"
-            style={{ display: "flex", alignItems: "center", marginLeft: "5%" }}
-          >
-            Лого
-          </div>
-          <div className="drop_down_title">
-            <span
-              style={{
-                marginTop: "1%",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              Запросы на выплату
-            </span>
-          </div>
-          <div
-            className="drop_down_button"
-            onClick={() => setShowNavbar((prev) => !prev)}
-          >
-            {showNavbar ? (
-              <CloseIcon fontSize="large" />
-            ) : (
-              <MenuIcon fontSize="large" />
-            )}
-          </div>
-        </div>
-
-        <div className="checkouts_buttons">
-          <Link
-            to={"/revenue"}
-            style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              textDecoration: "none",
-              color: "var(--tg-theme-button-text-color)",
-            }}
-          >
-            <button>Доходность</button>
-          </Link>
-        </div>
-      </div>
-      <div className="revenue_container">
-        <div style={{ width: "100%", height: "50%" }}>
-          <table border="2" className="checkouts_table">
-            <tbody>
-              <tr>
-                <td>Дата создания запроса</td>
-                <td>Сумма вывода</td>
-                <td>Статус запроса</td>
-              </tr>
-              <tr>
-                <td>28/9/2023</td>
-                <td>1800 ₽</td>
-                <td>Обработан</td>
-              </tr>
-              <tr>
-                <td>27/9/2023</td>
-                <td>1800 ₽</td>
-                <td>В обработке</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <CheckoutsDropDown
+        setShowNavbar={setShowNavbar}
+        showNavbar={showNavbar}
+      />
+      <CheckoutsTable />
     </div>
   );
 };
---
+++ src/pages/checkouts/components/CheckoutsTable.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function CheckoutsTable() {
+    return (
+        <div className="revenue_container">
+            <div style={{ width: "100%", height: "50%" }}>
+                <table border="2" className="checkouts_table">
+                    <tbody>
+                        <tr>
+                            <td>Дата создания запроса</td>
+                            <td>Сумма вывода</td>
+                            <td>Статус запроса</td>
+                        </tr>
+                        <tr>
+                            <td>28/9/2023</td>
+                            <td>1800 ₽</td>
+                            <td>Обработан</td>
+                        </tr>
+                        <tr>
+                            <td>27/9/2023</td>
+                            <td>1800 ₽</td>
+                            <td>В обработке</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutsTable
