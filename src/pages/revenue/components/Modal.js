3c65603428ac6cf43862ed3d6790231b7794820e
GavristovDenis <102148379+GavristovDenis@users.noreply.github.com>
2023-09-03 12:30:14 +0300

    fvgbhmkl

--- src/pages/revenue/components/Modal.js
+++ src/pages/revenue/components/Modal.js
@@ -8,10 +8,10 @@ function Modal({ setShowModal }) {
                     <div onClick={() => setShowModal((prev) => !prev)}>
                     </div>
                 </div>
-                <span>Введите сумму вывода:</span>
+                <span>Введите сумму вывода</span>
                 <input className="modal_input" placeholder="Введите сумму" />
                 <span>Сумма вывода не может превышать возможную сумму вывода</span>
-                <button className='modal_button_inactive'>Доступно к выводу: 321</button>
+                <button className='modal_button_inactive'>Доступно к выводу</button>
                 <button className="modal_button_active">Запросить выплату</button>
             </div>
         </div>
