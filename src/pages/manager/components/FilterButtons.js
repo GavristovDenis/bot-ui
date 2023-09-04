b78207fa42c7b0244ae6d507cef3ab6519ae7a7d
GavristovDenis <102148379+GavristovDenis@users.noreply.github.com>
2023-09-04 20:08:33 +0300

    sadfdggjhjk

--- src/pages/manager/components/FilterButtons.js
+++ src/pages/manager/components/FilterButtons.js
@@ -4,6 +4,16 @@ function FilterButtons({ setStatusDropDown, setMockData, statusFilter, data, dat
     return (
         <div className="filter_buttons_container">
             <div className="filter_buttons">
+
+
+                <div className='button_selector'>
+                    <div className='selector_button_container'> <button onClick={(e) => setSortDropDown(sortDropDown ? false : true) & e.stopPropagation()}>Сортировать по</button></div>
+                    {sortDropDown ? <div className='hidden_buttons'>
+                        <button onClick={() => dateSort() & setSortDropDown(prev => !prev)}>Дате</button>
+                        <button onClick={() => setMockData(data) & setSortDropDown(prev => !prev)}>-</button>
+                    </div> : " "}
+
+                </div>
                 <div className='button_selector'>
                     <div className='selector_button_container'>   <button onClick={(e) => setStatusDropDown(statusDropDown ? false : true) & e.stopPropagation()}>
                         Фильтровать по статусу
@@ -17,15 +27,6 @@ function FilterButtons({ setStatusDropDown, setMockData, statusFilter, data, dat
                             {" "}
                             Обработан
                         </button>
-                    </div> : " "}
-
-                </div>
-
-                <div className='button_selector'>
-                    <div className='selector_button_container'> <button onClick={(e) => setSortDropDown(sortDropDown ? false : true) & e.stopPropagation()}>Сортировать по</button></div>
-                    {sortDropDown ? <div className='hidden_buttons'>
-                        <button onClick={() => dateSort() & setSortDropDown(prev => !prev)}>Дате</button>
-                        <button onClick={() => setMockData(data) & setSortDropDown(prev => !prev)}>-</button>
                     </div> : " "}
 
                 </div>
